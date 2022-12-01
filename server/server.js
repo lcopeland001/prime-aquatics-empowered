af22eaf96b0c099b8c8975fdd667187151375134
Nathan Gaede <n.d.gaede@gmail.com>
2022-11-30 20:29:16 -0600

    Changed ports and commented out one incomplete facility saga

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use("/api/result", resultRouter);
 app.use(express.static("build"));
 
 // App Set //
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5008;
 
 /** Listen * */
 app.listen(PORT, () => {
