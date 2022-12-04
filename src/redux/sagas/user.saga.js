fdbb154f88895d190ad8ea01c3eeb12e03f2eb0d
Tu Nguyen <nguy1581@gmail.com>
2022-12-04 13:04:10 -0600

    Added User Profile, ability to edit user profile details (name, phone number), added a PUT saga for user, added PUT code for user.router.js (server)

--- src/redux/sagas/user.saga.js
+++ src/redux/sagas/user.saga.js
@@ -1,31 +1,44 @@
-import axios from 'axios';
-import { put, takeLatest } from 'redux-saga/effects';
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects";
 
 // worker Saga: will be fired on "FETCH_USER" actions
 function* fetchUser() {
-  try {
-    const config = {
-      headers: { 'Content-Type': 'application/json' },
-      withCredentials: true,
-    };
+    try {
+        const config = {
+            headers: { "Content-Type": "application/json" },
+            withCredentials: true,
+        };
 
-    // the config includes credentials which
-    // allow the server session to recognize the user
-    // If a user is logged in, this will return their information
-    // from the server session (req.user)
-    const response = yield axios.get('/api/user', config);
+        // the config includes credentials which
+        // allow the server session to recognize the user
+        // If a user is logged in, this will return their information
+        // from the server session (req.user)
+        const response = yield axios.get("/api/user", config);
 
-    // now that the session has given us a user object
-    // with an id and username set the client-side user object to let
-    // the client-side code know the user is logged in
-    yield put({ type: 'SET_USER', payload: response.data });
-  } catch (error) {
-    console.log('User get request failed', error);
-  }
+        // now that the session has given us a user object
+        // with an id and username set the client-side user object to let
+        // the client-side code know the user is logged in
+        yield put({ type: "SET_USER", payload: response.data });
+    } catch (error) {
+        console.log("User get request failed", error);
+    }
+}
+
+function* updateUser(action) {
+    try {
+        yield axios.put(`/api/user/${action.payload.id}`, action.payload);
+        yield put({
+            type: "FETCH_USER",
+        });
+    } catch (error) {
+        console.log("Error updating user profile", error);
+        alert("Something went wrong updating user profile");
+    }
 }
 
 function* userSaga() {
-  yield takeLatest('FETCH_USER', fetchUser);
+    yield takeLatest("FETCH_USER", fetchUser);
+    yield takeLatest("PUT_USER", updateUser);
 }
 
 export default userSaga;
