dcffadc18a72782a2fe5d6a7b3e1623e78e6c7fd
Keith Cullinan <kfcullinan@gmail.com>
2022-12-11 12:23:58 -0600

    capitalized all CL elements in test page

--- src/components/WaterTestPage/WaterTestPage.jsx
+++ src/components/WaterTestPage/WaterTestPage.jsx
@@ -77,7 +77,7 @@ function WaterTestPage() {
                         />
                     </p>
                     <p>
-                        Free Cl:{" "}
+                        Free CL:{" "}
                         <input
                             value={free_cl}
                             onChange={(e) => setFree_cl(e.target.value)}
@@ -85,7 +85,7 @@ function WaterTestPage() {
                         />
                     </p>
                     <p>
-                        Combined Cl:{" "}
+                        Combined CL:{" "}
                         <input
                             value={combined_cl}
                             onChange={(e) => setCombined_cl(e.target.value)}
@@ -93,7 +93,7 @@ function WaterTestPage() {
                         />
                     </p>
                     <p>
-                        Total Cl:{" "}
+                        Total CL:{" "}
                         <input
                             value={total_cl}
                             onChange={(e) => setTotal_cl(e.target.value)}
