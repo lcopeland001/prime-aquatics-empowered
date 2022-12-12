16455fa6d49b18c1b821a133bb61d912d94c509b
Raul Roldan <raul.ji@outlook.com>
2022-12-11 22:34:43 -0600

    added alets of success after editing a profile, create or edit a facility

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import "../App/App.css";
+import swal from "sweetalert";
 
 function EditProfile() {
     const dispatch = useDispatch();
@@ -22,6 +23,7 @@ function EditProfile() {
     }, [user]);
 
     const updateProfile = (event) => {
+        swal("User Updated!", "Success", "success");
         event.preventDefault();
         console.log("Editing profile");
         dispatch({
--- src/components/ManageUser/ManageUser.jsx
+++ src/components/ManageUser/ManageUser.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import "../App/App.css";
+import swal from "sweetalert";
 
 function ManageUser() {
     const dispatch = useDispatch();
